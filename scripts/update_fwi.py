dcdf1c80d89a60c40f25a76020ed647327172027
hashempoorsadegh214-prog <hashempoor.sadegh214@gmail.com>
2026-08-24 14:04:14 +0330

    Update update_fwi.py

--- scripts/update_fwi.py
+++ scripts/update_fwi.py
@@ -1,1 +1,11 @@
+import sys
+import os
 
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, ROOT)
+
+import fwi_fars
+
+
+if __name__ == "__main__":
+    fwi_fars.main()
